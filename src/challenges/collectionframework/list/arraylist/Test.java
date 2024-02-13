7bc59973f35c441430fe69235702b303de3e3007
Razvan <razvantrofin743@yahoo.com>
2024-02-13 23:04:49 +0200

    List

---
+++ src/challenges/collectionframework/list/arraylist/Test.java
@@ -0,0 +1,19 @@
+package challenges.collectionframework.list.arraylist;
+//Write a Java program to test an array list is empty or not.
+
+public class Test {
+
+    public static void main(String[] args) {
+        int[] arr = {};
+        System.out.println(has(arr));
+    }
+
+    public static boolean has(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
